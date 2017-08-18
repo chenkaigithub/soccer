44a0035a70b5e81ca043d17cd4de7de4738567c7
EthanDivestar <divestar@divestardeMacBook-Pro-3.local>
2017-08-18 11:47:05 +0800

    db settings

--- download/settings.py
+++ download/settings.py
@@ -1,7 +1,7 @@
 TRENDS_URL  = "http://sina.aicai.com/jczq/"
 RESULTS_URL = 'http://sina.aicai.com/lotnew/jc/getMatchByDate.htm?lotteryType=jczq&cate=gd&dataStr={date}'
 
-DB_USER = 'root'
-DB_PASS = 'root'
+DB_USER = 'ethan'
+DB_PASS = 'qwerasdf'
 DB_HOST = 'localhost'
 DB_NAME = 'soccer'
